8a187963e1cda6a5a43a2367a9d7e264643db9d7
snehakweera77 <snehakweera77@gmail.com>
2021-08-07 11:01:31 +0530

    View Binding

--- app/src/main/java/com/example/musicplayer/FavouriteActivity.kt
+++ app/src/main/java/com/example/musicplayer/FavouriteActivity.kt
@@ -2,10 +2,14 @@ package com.example.musicplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.musicplayer.databinding.ActivityFavouriteBinding
 
 class FavouriteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFavouriteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_favourite)
+        setTheme(R.style.Theme_MusicPlayer)
+        binding = ActivityFavouriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/musicplayer/PlaylistActivity.kt
+++ app/src/main/java/com/example/musicplayer/PlaylistActivity.kt
@@ -2,10 +2,13 @@ package com.example.musicplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.musicplayer.databinding.ActivityPlaylistBinding
 
 class PlaylistActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPlaylistBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_playlist)
-    }
+        setTheme(R.style.Theme_MusicPlayer)
+        binding = ActivityPlaylistBinding.inflate(layoutInflater)
+        setContentView(binding.root)    }
 }
