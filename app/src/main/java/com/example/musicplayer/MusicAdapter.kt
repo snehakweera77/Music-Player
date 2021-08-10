7ad5d673dddeaebe017ac85b651c29e97f8876cb
snehakweera77 <snehakweera77@gmail.com>
2021-08-10 22:57:01 +0530

    play pause btn and play songs

--- app/src/main/java/com/example/musicplayer/MusicAdapter.kt
+++ app/src/main/java/com/example/musicplayer/MusicAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.musicplayer
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -14,7 +16,7 @@ class MusicAdapter(private val context:Context, private val musicList: ArrayList
         val album = binding.songAlbumMV
         val image = binding.imageMV
         val duration = binding.songDuration
-
+        val root = binding.root
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
@@ -24,11 +26,17 @@ class MusicAdapter(private val context:Context, private val musicList: ArrayList
     override fun onBindViewHolder(holder: MyHolder, position: Int) {
         holder.title.text = musicList[position].title
         holder.album.text = musicList[position].album
-        holder.duration.text = musicList[position].duration.toString()
+        holder.duration.text = formatDuration(musicList[position].duration)
         Glide.with(context)
             .load(musicList[position].artUri)
             .apply(RequestOptions().placeholder(R.drawable.ic_back_icon).centerCrop())
             .into(holder.image)
+        holder.root.setOnClickListener{
+            val intent = Intent(context, PlayerActivity::class.java)
+            intent.putExtra("index", position)
+            intent.putExtra("class", "MusicAdapter")
+            ContextCompat.startActivity(context, intent,null)
+        }
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/musicplayer/PlayerActivity.kt
+++ app/src/main/java/com/example/musicplayer/PlayerActivity.kt
@@ -1,14 +1,72 @@
 package com.example.musicplayer
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.musicplayer.databinding.ActivityPlayerBinding
 
 class PlayerActivity : AppCompatActivity() {
+    companion object {
+        lateinit var musicListPA : ArrayList<Music>
+        var songPosition: Int = 0
+        var mediaPlayer: MediaPlayer?= null
+        var isPlaying: Boolean = false
+    }
     private lateinit var binding: ActivityPlayerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.coolPink)
         binding = ActivityPlayerBinding.inflate(layoutInflater)
-        setContentView(binding.root)    }
+        setContentView(binding.root)
+        initializeLayout()
+        binding.playPauseBtn.setOnClickListener{
+            if (isPlaying)
+                pauseMusic()
+            else
+                playMusic()
+        }
+        }
+    private fun setLayout() {
+        Glide.with(this)
+            .load(musicListPA[songPosition].artUri)
+            .apply(RequestOptions().placeholder(R.drawable.ic_back_icon).centerCrop())
+            .into(binding.songImgPA)
+        binding.songNamePA.text = musicListPA[songPosition].title
+    }
+    private fun createMediaPlayer(){
+        try {
+            if (mediaPlayer == null) mediaPlayer = MediaPlayer()
+            mediaPlayer!!.reset()
+            mediaPlayer!!.setDataSource(musicListPA[songPosition].path)
+            mediaPlayer!!.prepare()
+            mediaPlayer!!.start()
+            isPlaying = true
+            binding.playPauseBtn.setIconResource(R.drawable.ic_pause_icon)
+        } catch (e: Exception){return}
+    }
+    private fun initializeLayout(){
+        PlayerActivity.songPosition = intent.getIntExtra("index", 0)
+        when(intent.getStringExtra("class")){
+            "MusicA dapter" -> {
+                PlayerActivity.musicListPA = ArrayList()
+                PlayerActivity.musicListPA.addAll(MainActivity.MusicListMA)
+                setLayout()
+                createMediaPlayer()
+            }
+    }
+}
+private fun playMusic() {
+    binding.playPauseBtn.setIconResource(R.drawable.ic_pause_icon)
+    isPlaying = true
+    mediaPlayer!!.start()
+}
+
+    private fun pauseMusic() {
+        binding.playPauseBtn.setIconResource(R.drawable.ic_play_icon)
+        isPlaying = false
+        mediaPlayer!!.stop()
+    }
+
 }
